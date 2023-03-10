3d3124905801658aa521391f26a1edd73cc77929
Jacob <j31c0@hotmail.com>
2023-03-09 23:47:21 -0500

    Show gameboard values

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ const GameBoard = (() => {
     [0, 0, 0],
     [0, 0, 0],
   ];
+  const showBoard = () => {
+    gameBoard.forEach((value) => {
+      const container = document.querySelector(".container");
+      const elem = document.createElement("p");
+      elem.textContent = value;
+      container.appendChild(elem);
+      // const showIt = document.querySelector("p");
+      // showIt.textContent() = value;
+    });
+  };
+  return {showBoard}
 })();
 
 const Player = (name) => {
-    console.log(name);
-    return {name}
-}
+  console.log(name);
+  return { name };
+};
+
+GameBoard.showBoard();
